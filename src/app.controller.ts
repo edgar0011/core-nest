b95a3b1dba167a04f656e78adba8bcb84261f790
martinweiser <martin.weiser@gmail.com>
2021-01-14 19:45:45 +0100

    dev: controller updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(['', '/*'])
+  whatever(): string {
+    return this.appService.getDefaultAppMessage();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getDefaultAppMessage(): string {
+    return 'This is defualt app message';
+  }
 }
